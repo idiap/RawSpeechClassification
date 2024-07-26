dd0e797aa4b7a835ceea2709b152f6416e5a061b
Olivier Canévet <olivier.canevet@idiap.ch>
2024-07-26 15:50:26 +0200

    feat: Replace Flatten by GlobalAveragePooling1D

--- steps/model_architecture.py
+++ steps/model_architecture.py
@@ -3,18 +3,18 @@
 ## Copyright (c) 2018 Idiap Research Institute, http://www.idiap.ch/
 ## Written by S. Pavankumar Dubagunta <pavankumar [dot] dubagunta [at] idiap [dot] ch>
 ## and Mathew Magimai Doss <mathew [at] idiap [dot] ch>
-## 
+##
 ## This file is part of RawSpeechClassification.
-## 
+##
 ## RawSpeechClassification is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License version 3 as
 ## published by the Free Software Foundation.
-## 
+##
 ## RawSpeechClassification is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ## GNU General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with RawSpeechClassification. If not, see <http://www.gnu.org/licenses/>.
 
@@ -35,7 +35,8 @@ def model_architecture (arch, inputFeatDim=4000, outputFeatDim=1):
                 keras.layers.Activation('relu'),
                 keras.layers.Conv1D(filters=512, kernel_size=3, strides=1),
                 keras.layers.Activation('relu'),
-                keras.layers.Flatten(),
+                # keras.layers.Flatten(),
+                keras.layers.GlobalAveragePooling1D(),
                 keras.layers.Dense(10),
                 keras.layers.Activation('relu')])
 
@@ -51,13 +52,14 @@ def model_architecture (arch, inputFeatDim=4000, outputFeatDim=1):
                 keras.layers.Activation('relu'),
                 keras.layers.Conv1D(filters=512, kernel_size=3, strides=1),
                 keras.layers.Activation('relu'),
-                keras.layers.Flatten(),
+                # keras.layers.Flatten(),
+                keras.layers.GlobalAveragePooling1D(),
                 keras.layers.Dense(10),
                 keras.layers.Activation('relu')])
 
     else:
         raise TypeError('Unknown architecture: '+arch)
- 
+
     ## Add the final layer
     m.add(keras.layers.Dense(outputFeatDim))
     if outputFeatDim == 1:
