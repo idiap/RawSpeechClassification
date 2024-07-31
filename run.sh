0713d9f19fcd835bfc6a5b5086947be0c61fe8cf
Olivier Canévet <olivier.canevet@idiap.ch>
2024-07-31 10:37:37 +0200

    chore: Change model extension

--- run.sh
+++ run.sh
@@ -5,18 +5,18 @@
 ## Copyright (c) 2018 Idiap Research Institute, http://www.idiap.ch/
 ## Written by S. Pavankumar Dubagunta <pavankumar [dot] dubagunta [at] idiap [dot] ch>
 ## and Mathew Magimai Doss <mathew [at] idiap [dot] ch>
-## 
+##
 ## This file is part of RawSpeechClassification.
-## 
+##
 ## RawSpeechClassification is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License version 3 as
 ## published by the Free Software Foundation.
-## 
+##
 ## RawSpeechClassification is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ## GNU General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU General Public License
 ## along with RawSpeechClassification. If not, see <http://www.gnu.org/licenses/>.
 
@@ -46,9 +46,9 @@ test_feat=feat/test_feat
 [ -d $test_feat ] || ./steps/wav2feat.py $test_list $test_feat "test"
 
 ## Train
-[ -f $exp/cnn.h5 ] || ./steps/train.py $train_feat $cv_feat $exp $arch
-[ ! -f $exp/cnn.h5 ] && echo "Training failed. Check logs." && exit 1
+[ -f $exp/cnn.keras ] || ./steps/train.py $train_feat $cv_feat $exp $arch
+[ ! -f $exp/cnn.keras ] && echo "Training failed. Check logs." && exit 1
 
 ## Test
-[ -s $exp/scores.txt ] || ./steps/test.py $test_feat $exp/cnn.h5 > $exp/scores.txt
+[ -s $exp/scores.txt ] || ./steps/test.py $test_feat $exp/cnn.keras > $exp/scores.txt
 [ ! -s $exp/scores.txt ] && echo "Testing failed. Check logs." && exit 1
