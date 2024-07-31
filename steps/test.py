8cd1353f6929a79832240c54b1bd78c6553fd9a2
Olivier Canévet <olivier.canevet@idiap.ch>
2024-07-31 13:16:06 +0200

    style: Blackify code

--- steps/test.py
+++ steps/test.py
@@ -21,27 +21,33 @@
 
 
 import sys
+
 import keras
 import numpy
+
 from rawGenerator import rawGenerator
 
-def test (test_dir, model):
-    r = rawGenerator(test_dir, mode='test')
-    m = keras.models.load_model (model)
+
+def test(test_dir, model):
+    r = rawGenerator(test_dir, mode="test")
+    m = keras.models.load_model(model)
 
     spk_scores = {}
     spk_labels = {}
     spk_counts = {}
-    for w,feat,l in r:
+    for w, feat, l in r:
         pred = m.predict(feat)
-        spk = w ## Get the speaker ID. This is useful when each speaker has multiple utterances and
-                ## the results need to be calculated per speaker instead of per utterance. You need
-                ## to configure this line according how the speaker ID can be extracted from you data.
-                ## For e.g. the below line assumes that the basenames of the files start with speaker ID
-                ## followed by an utterance ID, separated by a '_'.
-                ## spk = w.split('/')[-1].split('_')[0]
-                ## By default, we use the wav file name as the speaker ID, which means that
-                ## each wav file corresponds to one speaker.
+
+        ## Get the speaker ID. This is useful when each speaker has multiple utterances and
+        ## the results need to be calculated per speaker instead of per utterance. You need
+        ## to configure this line according how the speaker ID can be extracted from you data.
+        ## For e.g. the below line assumes that the basenames of the files start with speaker ID
+        ## followed by an utterance ID, separated by a '_'.
+        ## spk = w.split('/')[-1].split('_')[0]
+        ## By default, we use the wav file name as the speaker ID, which means that
+        ## each wav file corresponds to one speaker.
+        spk = w
+
         if spk not in spk_scores:
             spk_scores[spk] = numpy.sum(pred, axis=0)
             spk_counts[spk] = len(pred)
@@ -53,10 +59,11 @@ def test (test_dir, model):
             spk_counts[spk] += len(pred)
 
     for spk in spk_labels:
-        print (spk, spk_labels[spk], spk_scores[spk]/spk_counts[spk])
+        print(spk, spk_labels[spk], spk_scores[spk] / spk_counts[spk])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test_dir = sys.argv[1]
     model = sys.argv[2]
 
-    test (test_dir, model)
+    test(test_dir, model)
