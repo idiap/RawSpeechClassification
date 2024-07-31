644cae433cf2cffd1d1a5b269696a000bbfdecd3
Olivier Canévet <olivier.canevet@idiap.ch>
2024-07-31 13:13:45 +0200

    style: Blackify code

--- steps/rawdataset.py
+++ steps/rawdataset.py
@@ -20,15 +20,16 @@
 ## along with RawSpeechClassification. If not, see <http://www.gnu.org/licenses/>.
 
 
-import pickle
-import numpy
-import h5py
 import os
+import pickle
+
+import h5py
 import keras
+import numpy as np
 
 
 class RawDataset(keras.utils.PyDataset):
-    def __init__ (self, featDir, batchSize=256, spliceSize=25, mode='train', **kwargs):
+    def __init__(self, featDir, batchSize=256, spliceSize=25, mode="train", **kwargs):
         kwargs = {"workers": 1}
         super().__init__(**kwargs)
         self.featDir = featDir
@@ -37,55 +38,61 @@ class RawDataset(keras.utils.PyDataset):
         self.mode = mode
 
         self.stdFloor = 1e-3
-        self.context = (self.spliceSize-1)//2
-        infoFile = self.featDir + '/info.npy'
-        self.info = numpy.load(infoFile, allow_pickle=True).item()
+        self.context = (self.spliceSize - 1) // 2
+        infoFile = self.featDir + "/info.npy"
+        self.info = np.load(infoFile, allow_pickle=True).item()
 
         ## Set attributes from info
-        self.numUtterances = self.info['numUtterances']
-        self.numFeats = self.info['numFeats']
-        self.numLabels = self.info['numLabels']
-        self.numSplit = self.info['numSplit']
-        self.inputFeatDim = self.info['inputFeatDim']*self.spliceSize
-        self.outputFeatDim = self.info['outputFeatDim']
+        self.numUtterances = self.info["numUtterances"]
+        self.numFeats = self.info["numFeats"]
+        self.numLabels = self.info["numLabels"]
+        self.numSplit = self.info["numSplit"]
+        self.inputFeatDim = self.info["inputFeatDim"] * self.spliceSize
+        self.outputFeatDim = self.info["outputFeatDim"]
 
         ## Compute number of steps
-        self.numSteps = -(-self.numFeats//self.batchSize)
+        self.numSteps = -(-self.numFeats // self.batchSize)
         self.numDone = 0
 
-        numpy.random.seed(512)
+        np.random.seed(512)
         self.splitDataCounter = 0
 
-        self.x = numpy.empty ((0, self.inputFeatDim), dtype=numpy.float32)
-        self.y = numpy.empty (0, dtype=numpy.int32)
+        self.x = np.empty((0, self.inputFeatDim), dtype=np.float32)
+        self.y = np.empty(0, dtype=np.int32)
         self.batchPointer = 0
         self.doUpdateSplit = True
 
-    def addContextNorm (self, feat):
+    def addContextNorm(self, feat):
         ## Add context to get the window size
         N = len(feat)
 
         ## Repeat feat[0], feat[-1] so that we get the same number of spliced feats
-        feat = numpy.concatenate([numpy.tile(feat[0], (self.context,1)), \
-                feat, numpy.tile(feat[-1], (self.context,1))])
+        feat = np.concatenate(
+            [
+                np.tile(feat[0], (self.context, 1)),
+                feat,
+                np.tile(feat[-1], (self.context, 1)),
+            ]
+        )
 
-        feat = numpy.lib.stride_tricks.as_strided(feat, strides=feat.strides, \
-                shape=(N, self.inputFeatDim))
+        feat = np.lib.stride_tricks.as_strided(
+            feat, strides=feat.strides, shape=(N, self.inputFeatDim)
+        )
 
         std = feat.std(axis=-1)
-        std[std<self.stdFloor] = self.stdFloor
-        feat = ((feat.T - feat.mean(axis=-1))/std).T
+        std[std < self.stdFloor] = self.stdFloor
+        feat = ((feat.T - feat.mean(axis=-1)) / std).T
         return feat
 
-    def __len__ (self):
+    def __len__(self):
         return self.numSteps
 
     ## Retrieve a mini batch
-    def __getitem__ (self, idx):
+    def __getitem__(self, idx):
         # print(f"getitem {idx} numDone {self.numDone} numSteps {self.numSteps} numFeats {self.numFeats} batch {self.batchSize}")
         self.numDone += 1
-        if self.mode == 'train':
-            while (self.batchPointer + self.batchSize >= len (self.x)):
+        if self.mode == "train":
+            while self.batchPointer + self.batchSize >= len(self.x):
                 if not self.doUpdateSplit:
                     self.doUpdateSplit = True
                     break
@@ -93,24 +100,24 @@ class RawDataset(keras.utils.PyDataset):
                 self.splitDataCounter += 1
 
                 # print(f"Loading {self.featDir} {self.splitDataCounter}")
-                featFile = '{:s}/{:d}.x.h5'.format(self.featDir, self.splitDataCounter)
-                labelFile = '{:s}/{:d}.y.h5'.format(self.featDir, self.splitDataCounter)
+                featFile = "{:s}/{:d}.x.h5".format(self.featDir, self.splitDataCounter)
+                labelFile = "{:s}/{:d}.y.h5".format(self.featDir, self.splitDataCounter)
 
-                with h5py.File(featFile,'r') as f:
+                with h5py.File(featFile, "r") as f:
                     featList = [self.addContextNorm(f[i][()]) for i in f]
-                x = numpy.vstack(featList)
+                x = np.vstack(featList)
 
-                with h5py.File(labelFile,'r') as f:
+                with h5py.File(labelFile, "r") as f:
                     labelList = [f[i][()] for i in f]
-                y = numpy.hstack(labelList)
+                y = np.hstack(labelList)
 
-                self.x = numpy.concatenate ((self.x[self.batchPointer:], x))
-                self.y = numpy.concatenate ((self.y[self.batchPointer:], y))
+                self.x = np.concatenate((self.x[self.batchPointer :], x))
+                self.y = np.concatenate((self.y[self.batchPointer :], y))
                 self.batchPointer = 0
                 # print(f"x {self.x.shape} y {self.y.shape}")
                 ## Shuffle data
-                randomInd = numpy.array(range(len(self.x)))
-                numpy.random.shuffle(randomInd)
+                randomInd = np.array(range(len(self.x)))
+                np.random.shuffle(randomInd)
                 self.x = self.x[randomInd]
                 self.y = self.y[randomInd]
 
@@ -118,21 +125,23 @@ class RawDataset(keras.utils.PyDataset):
                     self.splitDataCounter = 0
                     self.doUpdateSplit = False
 
-            xMini = self.x[self.batchPointer:self.batchPointer+self.batchSize]
-            yMini = self.y[self.batchPointer:self.batchPointer+self.batchSize]
+            xMini = self.x[self.batchPointer : self.batchPointer + self.batchSize]
+            yMini = self.y[self.batchPointer : self.batchPointer + self.batchSize]
             self.batchPointer += self.batchSize
             return (xMini, yMini)
 
-        else: ## Test mode
+        else:  ## Test mode
             while True:
                 if self.doUpdateSplit:
                     self.splitDataCounter += 1
-                    uflFile = '{:s}/{:d}.pickle'.format(self.featDir, self.splitDataCounter)
-                    self.f = open(uflFile,'rb')
+                    uflFile = "{:s}/{:d}.pickle".format(
+                        self.featDir, self.splitDataCounter
+                    )
+                    self.f = open(uflFile, "rb")
                     self.doUpdateSplit = False
                 try:
-                    utt,feat,lab = pickle.load(self.f)
-                    return (utt,self.addContextNorm(feat),lab)
+                    utt, feat, lab = pickle.load(self.f)
+                    return (utt, self.addContextNorm(feat), lab)
                 except EOFError:
                     self.f.close()
                     self.doUpdateSplit = True
